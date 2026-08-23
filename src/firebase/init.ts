addce4d96f3921de652e9df2a9d99113f370edbe
Tristan <tristan.robet@gmail.com>
2026-08-23 19:04:45 +0200

    Change firebase port

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -9,5 +9,5 @@ export const db = getDatabase(app)
 const useEmulator =
   import.meta.env.DEV || new URLSearchParams(window.location.search).get('emulator') === '1'
 if (useEmulator) {
-  connectDatabaseEmulator(db, 'localhost', 9000)
+  connectDatabaseEmulator(db, 'localhost', 9001)
 }
